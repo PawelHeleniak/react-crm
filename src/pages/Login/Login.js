decb418582ec9ad3601f08c1c1fe34f225993c37
PawelHeleniak <pawel.heleniak01@gmail.com>
2022-10-07 20:43:52 +0200

    Handling input value from login page

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+export const Login = () => {
+  const [loginValue, setLoginValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [visibilityPassword, setVisibilityPassword] = useState(false);
+  const [rememberPassword, setRememberPassword] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  const handleValue = (e) => {
+    let value = e.currentTarget.value;
+    let inputObject = e.target.id;
+    console.log(value);
+    switch (inputObject) {
+      case 'userLogin':
+        setLoginValue(value)
+        break;
+
+      case 'userPassword':
+        setPasswordValue(value)
+        break;
+      case 'showPassword':
+        setVisibilityPassword(!visibilityPassword)
+        break;
+      case 'rememberPassword':
+        setRememberPassword(!rememberPassword)
+        break;
+      default:
+        console.log("Błąd");
+        break;
+    }
+  }
+
+  return (
+    <div className='container'>
+      <div className="formBox">
+        <form action='' method='post' onSubmit={handleSubmit}>
+          <h2>Sign in</h2>
+          <div className='inputBox'>
+            <label>Username</label>
+            <input type='text' name='login' id='userLogin' value={loginValue} onChange={handleValue} />
+          </div>
+          <div className='inputBox'>
+            <label>Password</label>
+            <input name='password' id='userPassword' type={visibilityPassword ? 'text' : 'password'} value={passwordValue} onChange={handleValue} />
+          </div>
+          <div className=''>
+            <label htmlFor="">Show password</label>
+            <input type="checkbox" id='showPassword' value={visibilityPassword} checked={visibilityPassword ? 'checked' : ''} onChange={handleValue} />
+          </div>
+          <div className=''>
+            <label htmlFor="">Remember me</label>
+            <input type="checkbox" id='rememberPassword' value={rememberPassword} checked={rememberPassword ? 'checked' : ''} onChange={handleValue} />
+          </div>
+          <div className='inputBox submit'>
+            <input type='submit' value='Submit' />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
