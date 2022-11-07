2955463b5091706b4b86ea642615242a2948950e
PawelHeleniak <pawel.heleniak01@gmail.com>
2022-11-07 17:05:22 +0100

    Shorted the login form code

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -28,27 +28,43 @@ export const Login = () => {
     // navigate('/home')
   }
 
+  const handleValue = (e) => {
+    const { type, value, name, checked } = e.target
+
+    if (type === "text" || type === "password") {
+      setValues({
+        ...values, [name]: value
+      })
+    }
+    if (type === "checkbox") {
+      setValues({
+        ...values, [name]: checked
+      })
+    }
+  }
+
+
   return (
     <div className='container'>
       <div className="formBox">
         <form action='/' method='POST' onSubmit={handleSubmit}>
           <h2>Sign in</h2>
           <div className='inputBox'>
-            <input type='text' name='login' id='userLogin' placeholder='Username' value={values.login} onChange={e => setValues({ ...values, login: e.target.value })} />
+            <input type='text' name='login' id='userLogin' placeholder='Username' value={values.login} onChange={handleValue} />
           </div>
           <div className='inputBox'>
-            <input type={values.visibilityPassword ? 'text' : 'password'} name='password' id='userPassword' placeholder='Password' value={values.password} onChange={e => setValues({ ...values, password: e.target.value })} />
+            <input type={values.visibilityPassword ? 'text' : 'password'} name='password' id='userPassword' placeholder='Password' value={values.password} onChange={handleValue} />
           </div>
           <div className='inputBox'>
             <div className="checkboxWrapper">
               <label htmlFor="">Show password</label>
-              <input type="checkbox" id='showPassword' value={values.visibilityPassword} checked={values.visibilityPassword ? 'checked' : ''} onChange={e => setValues({ ...values, visibilityPassword: e.target.value })} />
+              <input type="checkbox" name='visibilityPassword' id='showPassword' checked={values.visibilityPassword} onChange={handleValue} />
             </div>
           </div>
           <div className='inputBox'>
             <div className="checkboxWrapper">
               <label htmlFor="">Remember me</label>
-              <input type="checkbox" id='rememberPassword' value={values.rememberPassword} checked={values.rememberPassword ? 'checked' : ''} onChange={e => setValues({ ...values, rememberPassword: e.target.value })} />
+              <input type="checkbox" name='rememberPassword' id='rememberPassword' checked={values.rememberPassword} onChange={handleValue} />
             </div>
           </div>
           <div className='inputBox submit'>
