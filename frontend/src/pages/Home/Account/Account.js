010b1ad5c4deda3743c276c61282aaa60a227bf3
PawelHeleniak <pawel.heleniak01@gmail.com>
2022-12-04 17:17:02 +0100

    improved view Account userData box

--- frontend/src/pages/Home/Account/Account.js
+++ frontend/src/pages/Home/Account/Account.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Calendar } from './Calendar'
+import photo from '../../../assets/img/default_account_96px.png';
 
 export const Account = (props) => {
   const { age, email, lastName, name, phone, profession, dateOfBirth } = props.userData;
@@ -10,12 +11,26 @@ export const Account = (props) => {
       <div className="wrapper">
         <div className="boxWrapper">
           <div className="box userData">
-            <h1>{name} {lastName}</h1>
-            <p><span>Age: </span>{age}</p>
-            <p><span>Date of birth: </span>{dateOfBirth}</p>
-            <p><span>Profession: </span>{profession}</p>
-            <p><span>Email: </span>{email}</p>
-            <p><span>Phone: </span>{phone}</p>
+            <div className="primaryInfo">
+              <div className="userPhoto">
+                <img src={photo} />
+              </div>
+              <div className="userName">
+                <h1>{name} {lastName}</h1>
+              </div>
+            </div>
+            <div className="generalInfo">
+              <div className="title">
+                <span>General</span>
+              </div>
+              <div className="information">
+                <p><span className='first'>Age</span><span>{age} years</span></p>
+                <p><span className='first'>Date of birth</span><span>{dateOfBirth}</span></p>
+                <p><span className='first'>Profession</span><span>{profession}</span></p>
+                <p><span className='first'>Email</span><span>{email}</span></p>
+                <p><span className='first'>Phone</span><span>{phone}</span></p>
+              </div>
+            </div>
           </div>
           <div className="box userSkills">
             <h1>Skills</h1>
